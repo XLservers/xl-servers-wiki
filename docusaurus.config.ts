5a9614cc13dc99f0b65e57bf4a1133364508225b
uertyk_ <victor.d.popov@gmail.com>
2025-02-02 23:14:58 +0400

    ddd

---
+++ docusaurus.config.ts
@@ -0,0 +1,136 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'XL-servers',
+  tagline: 'Давайте изучим мир мечты вместе!',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://github.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/xl-servers-wiki/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'XLservers', // Usually your GitHub org/user name.
+  projectName: 'xl-servers-wiki', // Usually your repo name.
+  deploymentBranch: "gh-pages",
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'XL-servers',
+      logo: {
+        alt: 'hyi na',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {to: '/blog', label: 'Блог (не настроен)', position: 'left'},
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Википедия',
+          items: [
+            {
+              label: 'Вики',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Сообщество',
+          items: [
+            {
+              label: 'Discord сервер',
+              href: 'https://discord.gg/D5uUTBZZRJ',
+            },
+          ],
+        },
+        {
+          title: 'Другое',
+          items: [
+            {
+              label: 'Блог',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub вики',
+              href: 'https://github.com/XLservers/xl-servers-wiki',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} XL-servers`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
