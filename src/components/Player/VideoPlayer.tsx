b86730358a882f6502c2cc1239df8f07cb62a0e7
uertyk_ <151397011+uertyk@users.noreply.github.com>
2025-02-03 13:18:15 +0400

    Копирование

--- src/components/Player/VideoPlayer.tsx
+++ src/components/Player/VideoPlayer.tsx
@@ -1,3 +1,82 @@
+import React, { useState, useRef, useEffect } from "react";
+import ReactPlayer from "react-player";
+import './video_player_style.css';
+
+interface PlayerProps {
+  url: string;
+}
+
+// OLD //
+/*
+export default function Player({ url }: PlayerProps) {
+  const [progress, setProgress] = useState(0);
+  const [playing, setPlaying] = useState(true);
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const playerRef = useRef<ReactPlayer>(null);
+  const playerContainerRef = useRef<HTMLDivElement>(null);
+
+  const togglePlayPause = () => {
+    setPlaying((prev) => !prev);
+  };
+
+  const handleSeek = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newProgress = parseFloat(e.target.value);
+    setProgress(newProgress);
+    playerRef.current?.seekTo(newProgress / 100, "fraction");
+  };
+
+  const handleFullscreenToggle = () => {
+    if (screenfull.isEnabled) {
+      screenfull.toggle(playerContainerRef.current);
+    }
+  };
+
+  useEffect(() => {
+    if (screenfull.isEnabled) {
+      screenfull.on("change", () => {
+        setIsFullscreen(screenfull.isFullscreen);
+      });
+
+      return () => {
+        screenfull.off("change", () => {
+          setIsFullscreen(screenfull.isFullscreen);
+        });
+      };
+    }
+  }, []);
+
+  return (
+    <div ref={playerContainerRef} className="relative w-full h-full"
+    style={{
+      position: "relative",
+      width: "100%",
+      height: "100%"
+    }}
+    >
+      <ProgressBar progress={progress} onSeek={handleSeek} />
+      <ReactPlayer
+        ref={playerRef}
+        url={url}
+        playing={playing}
+        loop
+        muted
+        playsInline={true}
+        playsinline={true}
+        controls={false}
+        width="100%"
+        height="100%"
+        progressInterval={100}
+        onProgress={(state) => setProgress(state.played * 100)}
+      />
+      <div className="opacity-0 hover:opacity-100 transition-opacity duration-300 w-full h-full flex items-center justify-center">
+        <div
+          className="absolute bottom-0 left-0 right-0 flex items-center justify-center cursor-pointer h-[97%]"
+          onClick={togglePlayPause}
+        >
+          <div>
+            <Icon
+              icon={playing ? "mdi:pause-circle" : "mdi:play-circle"}
+              fontSize={50}
               color="white"
             />
           </div>
@@ -107,4 +186,4 @@ function Bar({ progress, onSeek }: BarProps) {
       />
     </div>
   );
-  }
+}
