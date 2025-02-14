458cb68ea066cd0dee604231040ace3f840f7ba7
uertyk_ <victor.d.popov@gmail.com>
2025-02-14 11:47:24 +0400

    YAPPY, FUCK YOU

---
+++ astro.config.mjs
@@ -0,0 +1,53 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightImageZoom from 'starlight-image-zoom';
+import starlightSiteGraph from 'starlight-site-graph';
+import starlightThemeObsidian from 'starlight-theme-obsidian';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			plugins: [
+				starlightImageZoom({
+					// Configuration options go here.
+				}),
+				starlightSiteGraph()
+			],
+			title: 'XL-servers',
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
+			],
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Общее',
+					items: [
+						{
+							label: 'Правила',
+							items: [
+								{ label: 'Правила проекта', slug: 'all/rules' },
+								{ label: 'Дополнительно', slug: 'all/additionrules' },
+							],
+						},
+					],
+				},
+				{
+					label: 'Основное',
+					items: [
+						{
+							label: 'РП',
+							items: [
+								{ label: 'РП', slug: 'main/rp' },
+							],
+						},
+					],
+				},
+			],
+		}),
+	],
+});
